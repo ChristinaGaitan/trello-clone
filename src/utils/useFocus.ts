bb54d917571bf53ac0e1de7dcf19fb24b44aa3e0
Christina Gaitan <christina.gaitan@gmail.com>
2024-12-04 15:25:17 -0600

    Create useFocus hook and use it when creating a new item

---
+++ src/utils/useFocus.ts
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from "react"
+
+export const useFocus = () => {
+    const ref = useRef<HTMLInputElement>(null); // null marks the ref as readonly
+
+    useEffect(() => {
+        ref.current?.focus();
+    }, [])
+
+    return ref;
+}
