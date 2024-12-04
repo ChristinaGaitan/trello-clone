1090bf0789d7096e6c0ba324d2a86fa4b7682309
Christina Gaitan <christina.gaitan@gmail.com>
2024-12-04 14:22:49 -0600

    Create AddNewForm component

--- src/AddNewItem.tsx
+++ src/AddNewItem.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { AddItemButton } from './styles'
+import { NewItemForm } from './NewItemForm'
 
 type AddNewItemProps = {
     onAdd(text: string): void // callback function that will be called when we click the Create button
@@ -12,7 +13,14 @@ export const AddNewItem = (props: AddNewItemProps) => {
     const { onAdd, toggleButtonText, dark } = props;
 
     if (showForm) {
-        // We show item creation from here
+        return (
+            <NewItemForm
+                onAdd={(text) => {
+                    onAdd(text)
+                    setShowForm(false)
+                }}
+            />
+        )
     }
 
     return (
